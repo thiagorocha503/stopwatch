489112cf5057f3f2b253d5d6497d7e838c719096
Thiago Ferreira <thiagorocha503@gmail.com>
2023-05-04 15:02:49 -0300

    🚀 Deploy

--- js/display.ts
+++
@@ -1,8 +0,0 @@
-class Display {
-private display: HTMLSpanElement = document.getElementById("display") as HTMLSpanElement;
-
-    public setTime(minutes: number, seconds: number, milliseconds: number) {
-        this.display.innerHTML = leftPad(minutes, 2) + ":" + leftPad(seconds, 2) + "." +  leftPad(milliseconds, 3) 
-    }
-
-}
--- js/script.ts
+++
@@ -1,9 +0,0 @@
-$(() => {
-    const display = new Display();
-    const controller = new Control();
-    const app = new Stopwatch(display, controller);
-    document.getElementById("btn-start")?.addEventListener("click", ()=> app.play())
-    document.getElementById("btn-continue")?.addEventListener("click", ()=> app.continue())
-    document.getElementById("btn-pause")?.addEventListener("click", ()=> app.pause())
-    document.getElementById("btn-reset")?.addEventListener("click", ()=> app.reset())
-})
